2780501466f212f82aa7d44e0b5c199f9d6416ab
Tanner Gates <tgates4@dmail.dixie.edu>
2021-12-05 22:41:25 -0700

    Create Users, Navbar, and Spinner Components. Instal Axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
+import React, { Component } from "react"
+import "./App.css"
+import Navbar from "./components/layout/Navbar"
+import Users from "./components/users/Users"
+import Axios from "axios"
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const response = await Axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ users: response.data, loading: false })
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
